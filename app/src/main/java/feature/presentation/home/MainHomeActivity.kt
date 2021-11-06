b79b626d60ef1fb2e07da6d833c0b1d80db1142f
plinio.alencar <plinioprojetos@hotmail.com>
2021-11-06 17:26:31 -0300

    Removing useless comments on MainHome and lint

--- app/src/main/java/feature/presentation/home/MainHomeActivity.kt
+++ app/src/main/java/feature/presentation/home/MainHomeActivity.kt
@@ -11,12 +11,6 @@ import feature.presentation.HomeAdapter
 import feature.utils.navigateTo
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-/**
- * Os comentário nessa feature servem para padronizar e exemplificar o que será feito nas outras features
- * No app os comentário só serão bem vindos caso sejam de EXTREMA NECESSIDADE.
- * O ideal é que o código seja auto explicativo.
- */
-
 class MainHomeActivity : AppCompatActivity() {
     private val viewModel: HomeViewModel by viewModel()
     private lateinit var binding: ActivityMainHomeBinding
@@ -29,25 +23,15 @@ class MainHomeActivity : AppCompatActivity() {
         onEnterActivity()
     }
 
-    /**
-     * Função padrão para todas as activities, fragments ... telas no geral.
-     * Centraliza todas as funções que devem acontecer quando a tela é iniciada.
-     */
     private fun onEnterActivity() {
         populateViews()
         observers()
     }
 
-    /**
-     * Chama a função que busca a lista de apresentações para o recycler
-     */
     private fun populateViews() {
         viewModel.getFeaturesList()
     }
 
-    /**
-     * Observa o viewModel
-     */
     private fun observers() {
         viewModel.featureList.observe(
             this,
@@ -57,11 +41,6 @@ class MainHomeActivity : AppCompatActivity() {
         )
     }
 
-    /**
-     * Inicializa o adapter
-     * featurePresentation é a lista de features
-     * action: Função que realiza algo após o click no item do Recycler
-     */
     private fun setAdapter(featurePresentations: List<FeaturePresentation>) {
         val adapter = HomeAdapter(
             presentations = featurePresentations,
@@ -70,15 +49,6 @@ class MainHomeActivity : AppCompatActivity() {
         binding.homeRecycler.adapter = adapter
     }
 
-    /**
-     * Caso necessário
-     *Centraliza tudo que as views devem fazer ao serem acionadas(Caso sejam),
-     * exemplo: Listeners
-     */
-
-    /**
-     * Navega de acordo com o clique no item do recycler
-     */
     private fun startNavigation(featureFlag: FeatureFlag) = when (featureFlag) {
         RICKY_AND_MORTY -> navigateTo<RickyHomeActivity>()
         EXAMPLES -> navigateTo<ExampleSplashActivity>()
@@ -88,11 +58,4 @@ class MainHomeActivity : AppCompatActivity() {
         FeatureFlag.DOGS -> TODO()
         FeatureFlag.BILLS_TO_PAY -> TODO()
     }
-
-    /**
-     * Ao fazer o xml das telas certifique-se de centralizar as strings no arquivo strings.xml
-     * As strings ficarão no arquivo strings de cada módulo para ajudar a organizar.
-     * E dimensões (exceto tamanho de fonte) em dimens.xml na feature commons
-     * Cores também ficarão centralizadas em colors da feature commons
-     */
 }
--- feature-examples/src/main/java/feature/presentation/home/ExampleSplashActivity.kt
+++ feature-examples/src/main/java/feature/presentation/home/ExampleSplashActivity.kt
@@ -1,17 +1,19 @@
 package feature.presentation.home
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.example.feature_examples.R
 import com.example.feature_examples.databinding.ActivitySplashBinding
 
 /**
  * Responsible just for make a splash activity (presentation screen to the user)
  */
+@SuppressLint("CustomSplashScreen")
 class ExampleSplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
@@ -39,10 +41,13 @@ class ExampleSplashActivity : AppCompatActivity() {
          * right after this duration we automatically close this activity
          * and starts the next activity one
          */
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, ExamplesHomeActivity::class.java))
-            finish()
-        }, SPLASH_DURATION)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this, ExamplesHomeActivity::class.java))
+                finish()
+            },
+            SPLASH_DURATION
+        )
     }
 
     companion object {
