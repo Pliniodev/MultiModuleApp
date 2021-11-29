7dd5533f31924721920347cac08ae51b2c811178
plinio.alencar <plinioprojetos@hotmail.com>
2021-11-28 21:23:10 -0300

    adding serializable on data classes

--- feature-jsonplaceholder/src/main/java/feature/jsonplaceholder/domain/Post.kt
+++ feature-jsonplaceholder/src/main/java/feature/jsonplaceholder/domain/Post.kt
@@ -1,8 +1,10 @@
 package feature.jsonplaceholder.domain
 
+import java.io.Serializable
+
 internal data class Post(
     val body: String?,
     val id: Int?,
     val title: String?,
     val userId: Int?
-)
+) : Serializable
