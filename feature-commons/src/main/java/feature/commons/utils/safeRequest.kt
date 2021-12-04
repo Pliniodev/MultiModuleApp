216316ebc9534ae823d1740eba2c75b76b25f790
plinio.alencar <plinioprojetos@hotmail.com>
2021-12-04 11:02:29 -0300

    lint

--- feature-commons/src/main/java/feature/commons/utils/safeRequest.kt
+++ feature-commons/src/main/java/feature/commons/utils/safeRequest.kt
@@ -1,10 +1,7 @@
 package feature.commons.utils
 
-import com.google.gson.Gson
-import feature.commons.utils.ErrorResponse.Companion.EMPTY_API_ERROR
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import retrofit2.HttpException
 
 suspend inline fun <T> safeRequest(
     dispatcher: CoroutineDispatcher,
@@ -19,12 +16,13 @@ suspend inline fun <T> safeRequest(
     }
 }
 
-internal fun convertErrorBody(throwable: HttpException): ErrorResponse? {
-    try {
-        val errorJsonString = throwable.response()?.errorBody()?.string()
-        return Gson().fromJson(errorJsonString, ErrorResponse::class.java)
-    } catch (exception: Exception) {
-        null
-    }
-    return EMPTY_API_ERROR
-}
+// On tests
+// internal fun convertErrorBody(throwable: HttpException): ErrorResponse? {
+//    try {
+//        val errorJsonString = throwable.response()?.errorBody()?.string()
+//        return Gson().fromJson(errorJsonString, ErrorResponse::class.java)
+//    } catch (exception: Exception) {
+//        null
+//    }
+//    return EMPTY_API_ERROR
+// }
