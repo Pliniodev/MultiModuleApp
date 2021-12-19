453e1112630945f77a95e05a6b70981935d67895
Paulo Borges <premiersoft-paulo.borges@ailos.coop.br>
2021-12-19 19:34:13 -0300

    Adding a new layout for recycler view items.

--- feature-marvelapi/src/main/java/feature/marvelapi/presentation/home/adapter/MainMarvelAdapter.kt
+++ feature-marvelapi/src/main/java/feature/marvelapi/presentation/home/adapter/MainMarvelAdapter.kt
@@ -2,12 +2,11 @@ package feature.marvelapi.presentation.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import feature.commons.utils.BaseItemCallBack
 import feature.marvelapi.R
-import feature.marvelapi.databinding.MainRecyclerBinding
+import feature.marvelapi.databinding.CharactersCardRecyclerBinding
 import feature.marvelapi.presentation.model.CharactersPresentation
 
 internal class MainMarvelAdapter :
@@ -17,7 +16,7 @@ internal class MainMarvelAdapter :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemBinding =
-            MainRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            CharactersCardRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(itemBinding)
     }
 
@@ -25,15 +24,17 @@ internal class MainMarvelAdapter :
         holder.bind(getItem(position))
     }
 
-    class ViewHolder(private val binding: MainRecyclerBinding) :
+    class ViewHolder(private val binding: CharactersCardRecyclerBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: CharactersPresentation) {
 
+            val imagePath = "${item.thumbnail.path}/portrait_incredible.${item.thumbnail.extension}"
+
             binding.apply {
                 characterName.text = item.name
                 Glide.with(binding.root)
-                    .load("${item.thumbnail.path}.${item.thumbnail.extension}")
+                    .load(imagePath)
                     .placeholder(R.drawable.ic_baseline_portrait_24)
                     .centerCrop()
                     .into(characterImage)
